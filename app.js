4c43b6c9c815cd722eb8206b8b03a38079e5b9ef
MarkivNazar <nazarmn2008@gmail.com>
2025-05-06 21:10:28 +0300

    fixe

--- app.js
+++ app.js
@@ -33,5 +33,14 @@ app.post('/transactionsSave', async (req, res) => {
         
     });
 });
+
+
+
+
+
+
+
+
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
